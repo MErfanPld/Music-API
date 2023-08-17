4e8374cfb5744fc69456b78ab97a7b50d2916bac
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2023-08-17 22:59:59 +0330

    Add Permission music

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = "music"
 
 urlpatterns = [
-    path('', views.MusicListCreateAPIView.as_view(), name="music_list_create"),
-    path('<int:pk>/', views.MusicRetrieveUpdateDestroyAPIView.as_view(),
-         name="music_detail_update_delete"),
+    path('', views.MusicListAPIView.as_view(), name="music_list"),
+    path('', views.MusicCreateAPIView.as_view(), name="music_create"),
+    path('<int:pk>/', views.MusicRetrieveAPIView.as_view(), name="music_retrieve"),
+    path('<int:pk>/', views.MusicUpdateAPIView.as_view(), name="music_update"),
+    path('<int:pk>/', views.MusicDestroyAPIView.as_view(), name="music_delete"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
+from acl.mixins import *
 from .models import Music
 from . import serializers
 
 
 # Create your views here.
 
-class MusicListCreateAPIView(ListCreateAPIView):
-    # permission_classes = (IsAuthenticated,)
+class MusicListAPIView(PermissionMixin, ListAPIView):
+    permissions = ['music_list']
     queryset = Music.objects.filter(status=True)
     serializer_class = serializers.MusicSerializer
 
 
-class MusicRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    # permission_classes = (IsAuthenticated,)
+class MusicCreateAPIView(PermissionMixin, CreateAPIView):
+    permissions = ['music_create']
+    queryset = Music.objects.filter(status=True)
+    serializer_class = serializers.MusicSerializer
+
+
+class MusicRetrieveAPIView(PermissionMixin, RetrieveAPIView):
+    permissions = ['music_detail']
+    queryset = Music.objects.filter(status=True)
+    serializer_class = serializers.MusicSerializer
+
+
+class MusicUpdateAPIView(PermissionMixin, UpdateAPIView):
+    permissions = ['music_edit']
+    queryset = Music.objects.filter(status=True)
+    serializer_class = serializers.MusicSerializer
+
+
+class MusicDestroyAPIView(PermissionMixin, DestroyAPIView):
+    permissions = ['music_delete']
     queryset = Music.objects.filter(status=True)
     serializer_class = serializers.MusicSerializer
